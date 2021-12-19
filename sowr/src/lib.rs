dc14b2afa804ce5f52061d5c7804a1151ba8d82b
Khoi Nguyen <khoifish@gmail.com>
2021-12-19 14:00:32 +0700

    Make into library, Shirley-One-Weekend-Raytracer (sowr)

--- sowr/src/lib.rs
+++ sowr/src/lib.rs
@@ -1,25 +1,22 @@
-mod color;
-mod ray;
-mod vec3;
-mod hittable;
-mod sphere;
-mod camera;
-mod utils;
-mod material;
+pub mod color;
+pub mod ray;
+pub mod vec3;
+pub mod hittable;
+pub mod sphere;
+pub mod camera;
+pub mod utils;
+pub mod material;
 
 use std::rc::Rc;
-use std::io;
-use std::io::Write;
-use color::Color64;
-use ray::Ray;
 use vec3::Vec3;
 use hittable::{Hittable, HittableList};
 use sphere::Sphere;
-use camera::{Camera};
+use color::{Color64};
+use ray::{Ray};
 
 // --------------------------------------------------------------------------------------------------------------------
 
-fn random_scene() -> HittableList {
+pub fn random_scene() -> HittableList {
     let mut world = HittableList::default();
 
     // Ground
@@ -73,7 +70,7 @@ fn random_scene() -> HittableList {
 
 // --------------------------------------------------------------------------------------------------------------------
 
-fn ray_color(r : &Ray<f64>, world: &HittableList, depth: u32) -> Color64 {
+pub fn ray_color(r : &Ray<f64>, world: &HittableList, depth: u32) -> Color64 {
     if depth <= 0 {
         return Color64::default();
     }
@@ -99,54 +96,4 @@ fn ray_color(r : &Ray<f64>, world: &HittableList, depth: u32) -> Color64 {
     let t = 0.5 * (unit_direction.y() + 1.0);
 
     (Color64::new(1.0, 1.0, 1.0) * (1.0 - t)) + (Color64::new(0.5, 0.7, 1.0) * t)
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-pub fn main() {
-    // Image
-    let aspect_ratio = 3.0 / 2.0;
-    let image_width = 320_i32;
-    let image_height = ((image_width as f64) / aspect_ratio) as i32;
-    let samples_per_pixel = 500;
-    let max_depth = 50;
-
-    // World
-    let world = random_scene();
-
-    // Camera
-    let camera;
-    {
-        let look_from = Vec3::new(13.0, 2.0, 3.0);
-        let look_at = Vec3::new(0.0, 0.0, 0.0);
-        let up = Vec3::new(0.0, 1.0, 0.0);
-        let dist_to_focus = 10.0;
-        let aperture = 0.1;
-
-        camera = Camera::new(
-            &look_from,
-            &look_at,
-            &up,
-            90.0,
-            aspect_ratio,
-            aperture,
-            dist_to_focus
-        );
-    }
-
-    print!("P3\n{0} {1}\n255\n", image_width, image_height);
-
-    for j in (0..image_height).rev() {
-        for i in 0..image_width {
-            let mut pixel_color = Color64::default();
-            for _s in 0..samples_per_pixel {
-                let u = ((i as f64) + utils::random_range(0.0, 1.0)) / ((image_width - 1) as f64);
-                let v = ((j as f64) + utils::random_range(0.0, 1.0)) / ((image_height - 1) as f64);
-                let r = camera.get_ray(u, v);
-                pixel_color = pixel_color + ray_color(&r, &world, max_depth);
-            }
-            color::write_color64(&pixel_color, samples_per_pixel);
-        }
-    }
-    io::stdout().flush().unwrap();
 }
